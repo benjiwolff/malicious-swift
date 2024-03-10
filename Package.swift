23ad7399b25ca87bf62b81570ea22a67f1e4e3a3
Benjamin Wolff <benjamin.wolff@digitecgalaxus.ch>
2024-03-10 16:03:56 +0100

    initial commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import AppKit
+import PackageDescription
+
+let package = Package(
+    name: {
+        let process = Process()
+        process.launchPath = "/usr/bin/say"
+        process.arguments = ["hello world"]
+        process.launch()
+        return "malicious-swift"
+    }(),
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "malicious-swift",
+            targets: ["malicious-swift"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "malicious-swift"),
+    ]
+)
