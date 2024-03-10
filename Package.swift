4eeaeb5e2cfad70ba1a62a6706621e02332874b5
Benjamin Wolff <benjamin.wolff@digitecgalaxus.ch>
2024-03-10 16:36:39 +0100

    simplify package.swift

--- Package.swift
+++ Package.swift
@@ -4,25 +4,26 @@
 import AppKit
 import PackageDescription
 
-let package = Package(
-    name: {
-        let process = Process()
-        process.launchPath = "/usr/bin/say"
-        process.arguments = ["hello world"]
-        process.launch()
-        return "malicious-swift"
-    }(),
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "malicious-swift",
-            targets: ["malicious-swift"]
-        ),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "malicious-swift"),
-    ]
-)
+let package = {
+    let process = Process()
+    process.launchPath = "/usr/bin/say"
+    process.arguments = ["hello world"]
+    process.launch()
+
+    return Package(
+        name: "malicious-swift",
+        products: [
+            // Products define the executables and libraries a package produces, making them visible to other packages.
+            .library(
+                name: "malicious-swift",
+                targets: ["malicious-swift"]
+            ),
+        ],
+        targets: [
+            // Targets are the basic building blocks of a package, defining a module or a test suite.
+            // Targets can depend on other targets in this package and products from dependencies.
+            .target(
+                name: "malicious-swift"),
+        ]
+    )
+}
